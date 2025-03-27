34c848bb982272c1ab83f77714e394684016d866
Ahsan <ahsantahir9903@gmail.com>
2025-03-27 11:32:13 +0500

    Add files via upload

---
+++ GeminiChatBot.py
@@ -0,0 +1,50 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key="AIzaSyDBjwxFWTR8RchcwRCnKHchBccV1Qolzow")
+
+generation_config = {
+    "temperature": 0,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-2.0-flash",
+    safety_settings=safety_settings,
+    generation_config=generation_config,
+    system_instruction="this chatbot will answer after thinking a lot and concise answer please.",
+)
+
+chat_session = model.start_chat(history=[])
+
+print("Bot: What do you want?")
+print()
+
+while True:
+    user_input = input("You: ")
+    print()
+
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        print("Bot: Goodbye!")
+        break
+
+    response = chat_session.send_message(user_input)
+    model_response = response.text
+
+    print(f'Bot: {model_response}')
+    print()
+
+    chat_session.history.append({"role": "user", "parts": [user_input]})
+    chat_session.history.append({"role": "model", "parts": [model_response]})
